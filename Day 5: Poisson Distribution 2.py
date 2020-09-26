a607934c77e735b956ee36943e4187e4ba974bf3
DhrumilV <66421185+DhrumilV@users.noreply.github.com>
2020-09-26 13:41:57 +0530

    Create Day 5: Poisson Distribution 2.py

---
+++ Day 5: Poisson Distribution 2.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+m1, m2 = 0.88, 1.55
+
+c_a = 160 + ( 40 * (m1**2 + m1))
+c_b = 128 + ( 40 * (m2**2 + m2))
+
+print(round(c_a,3))
+print(round(c_b,3))
